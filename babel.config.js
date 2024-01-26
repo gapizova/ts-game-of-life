a01cdd1dc1f24331dfb5e5316cbee9b83548b747
gapizova <kat1502@mail.ru>
2024-01-26 13:16:42 +0500

    add jest for ts

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+  presets: [
+    [
+      '@babel/preset-env',
+      { targets: { node: 'current' } },
+      '@babel/preset-typescript',
+    ],
+  ],
   plugins: [
     'babel-plugin-styled-components',
     '@babel/plugin-proposal-class-properties',
