61b8c8375b6c6835000d77c0ff3b71fb40024b0a
gapizova <kat1502@mail.ru>
2024-01-29 11:41:26 +0500

    edit getNextState

--- src/getNextState.ts
+++ src/getNextState.ts
@@ -1,13 +1,13 @@
-import { getNumOfAliveNeighbours } from "./getNumOfAliveNeighbours";
-import { getCellState } from "./getCellState";
-import { getNewCellState } from "./getNewCellState";
+import {getNumOfAliveNeighbours} from "./getNumOfAliveNeighbours";
+import {getCellState} from "./getCellState";
+import {getNewCellState} from "./getNewCellState";
 
 /**
  * получить новое состояние
  * @param field {number[][]} - состояние поля
  * @return number[][] - новое состояние поля
  */
-export function getNextState(field) {
+export function getNextState(field: number[][]): number[][] {
   // return field.map((row, rowIndex) =>
   //   {
   //     return row.map((cell, cellIndex) =>
@@ -23,8 +23,7 @@ export function getNextState(field) {
     row.map((cell, cellIndex) => {
       const an = getNumOfAliveNeighbours(cellIndex, rowIndex, field);
       const currentState = getCellState(field, cellIndex, rowIndex);
-      const newState = getNewCellState(currentState, an);
-      return newState;
+        return getNewCellState(currentState, an);
     })
   );
 }
